224b908d37269dd3a82439d2baa42e114358cfdb
red <jakob.hillers@gmail.com>
2022-04-11 23:45:01 +0200

    [code] base installation for entering the machine

--- modules/base-install.sh
+++ modules/base-install.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-pacstrap /mnt base linux linux-firmware base-devil neovim wget git efibootmgr gzip sudo tree inetutils net-tools
+pacstrap /mnt base linux linux-firmware base-devil neovim wget git efibootmgr gzip sudo tree inetutils net-tools base-devel unzip vim nano
 genfstab -Z /mnt >> /mnt/etc/fstab
+wait 5
+echo "Entering your new machine...."
+arch-chroot /mnt
