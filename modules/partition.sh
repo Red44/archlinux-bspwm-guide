e33b592d4193bd571025bef1fa69742d02c096bb
red <jakob.hillers@gmail.com>
2022-04-11 15:09:25 +0200

    [code] home partition removed

--- modules/partition.sh
+++ modules/partition.sh
@@ -12,15 +12,12 @@ read -r operating_disk
 fdisk "/dev/$operating_disk"
 
 # Ask for partitions
-ask_for_name "Home"
-home_disk=$partition_name
 ask_for_name "System"
 system_disk=$partition_name
 ask_for_name "EFI"
 efi_disk=$partition_name
 
 # Format partitions
-mkfs.ext4 "/dev/$home_disk"
 mkfs.ext4 "/dev/$system_disk"
 mkfs.fat -F32 "/dev/$efi_disk"
 
@@ -30,9 +27,7 @@ mount "/dev/$home_system_disk /mnt"
 # Save collected data for futher use
 mkdir /mnt/install
 touch /mnt/install/efi
-touch /mnt/install/home
 
 echo "$efi_disk" > /mnt/install/efi
-echo "$home_disk" > /mnt/install/home
 
 echo "Partitioning done!"
