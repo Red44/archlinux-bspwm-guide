340746e62c20b44ad8258c891364b322662fb2d5
red <jakob.hillers@gmail.com>
2022-04-11 23:54:48 +0200

    [code] regions etc

---
+++ modules/system-defaults.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+pacman -Syu
+pacman -Sy neofetch
+function check_dir(){
+  if ! [ -d "$1/$2" ] || [ -z "$2" ]; then
+    echo "Invalid dir $2"
+    exit
+  fi
+}
+
+check_file(){
+  if ! [ -e "$1/$2" ] || [ -z "$2" ]; then
+    echo "Invalid file $2"
+  fi
+}
+read -p "Enter your region : " region
+check_dir "/usr/share/zoneinfo/" $region
+read -p "Enter your city : " city
+check_file "/usr/share/zoneinfo/$region/" $city
+ln -sf "/usr/share/zoneinfo/$region/" $city /etc/localtime
+hwclock --systohc
+nvim /etc/locale.gen
+
+
+
