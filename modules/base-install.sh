ffadc3119a01cc1acaa897f004ff70cb5a0eeae0
red <jakob.hillers@gmail.com>
2022-04-12 01:30:46 +0200

    [fix] wait not necessary

--- modules/base-install.sh
+++ modules/base-install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 pacstrap /mnt base linux linux-firmware base-devel neovim wget git efibootmgr gzip sudo tree inetutils net-tools unzip vim nano
 genfstab -U /mnt >> /mnt/etc/fstab
-wait 5
 echo "Entering your new machine...."
 arch-chroot /mnt
