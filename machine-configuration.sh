8bbe66e72be16716db22ddf92df3dce5f24b8519
red <jakob.hillers@gmail.com>
2022-04-11 23:55:30 +0200

    [config] pre arch-chroot

---
+++ machine-configuration.sh
@@ -0,0 +1,7 @@
+  modules/network.sh
+  modules/system-defaults.sh
+  modules/bootloader.sh
+  modules/graphic-drivers.sh
+  modules/user.sh
+  modules/desktop.sh
+  modules/some-development-tools.sh
