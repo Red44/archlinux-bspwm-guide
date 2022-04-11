1d159c830b829887cc3ec11492195be39ef7361d
red <jakob.hillers@gmail.com>
2022-04-12 00:34:31 +0200

    [fix] invalid package

--- modules/base-install.sh
+++ modules/base-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pacstrap /mnt base linux linux-firmware base-devil neovim wget git efibootmgr gzip sudo tree inetutils net-tools base-devel unzip vim nano
+pacstrap /mnt base linux linux-firmware base-devel neovim wget git efibootmgr gzip sudo tree inetutils net-tools unzip vim nano
 genfstab -Z /mnt >> /mnt/etc/fstab
 wait 5
 echo "Entering your new machine...."
