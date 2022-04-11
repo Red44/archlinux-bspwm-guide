c884e42189336b60cac0d2d0d086d07a1a24c6da
red <jakob.hillers@gmail.com>
2022-04-12 01:34:24 +0200

    [code] move scripts and data to new machine

--- modules/base-install.sh
+++ modules/base-install.sh
@@ -2,4 +2,5 @@
 pacstrap /mnt base linux linux-firmware base-devel neovim wget git efibootmgr gzip sudo tree inetutils net-tools unzip vim nano
 genfstab -U /mnt >> /mnt/etc/fstab
 echo "Entering your new machine...."
+mv * /mnt/root
 arch-chroot /mnt
