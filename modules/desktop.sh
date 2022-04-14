32256e8e56b01166603946d8e12c7f80a7c6b7c0
ReD <jakob.hillers@gmail.com>
2022-04-14 17:09:52 +0200

    [fix] tmp moved to local user to avoid permission iusses

--- modules/desktop.sh
+++ modules/desktop.sh
@@ -12,15 +12,14 @@ yay -Sy yay
 sudo pacman -Syu
 sudo pacman -Sy xorg xorg-xini bspwm sxhkd rofi nitrogen picom arandr neofetch firefox-developer-edition mplayer base-devel clang jdk8-openjdk python-pip pulseaudio powerline qalculate-gtk ranger kleopatra kolourpaint gimp btop libreoffice-still mlocate rxvt-unicode rxvt-unicode-terminfo urxvt-perls teamspeak3 discord xsel
 yay -Sy cava cbonsai downgrade fim openvpn3 polybar powerline-shell sublime-text-4 timeshift tty-clock nerd-fonts-source-code-pro
-sudo updatedb
-
 home="/home/$user"
-
-cd /tmp/ && git clone https://github.com/Red44/archlinux-bspwm-guide/
-mv "/tmp/archlinux-bspwm-guide/config/*" "$home/"
+mkdir ~/tmp
+cd ~/tmp/ && git clone https://github.com/Red44/archlinux-bspwm-guide/
+mv "~/tmp/archlinux-bspwm-guide/config/*" "$home/" -f
 xrdb "$home/.Xresources"
 echo "Wallpapers are in $home/.wallpapers"
 nitrogen
+sudo updatedb
 echo "Done"
 echo "Desktop shut start after login"
 sleep 10
