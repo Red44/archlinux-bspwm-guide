d76a4826542999790b8fc812a04f230a9ea64149
ReD <jakob.hillers@gmail.com>
2022-04-14 12:02:10 +0200

    [fix] local dir is now used

--- modules/uefi.sh
+++ modules/uefi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pacman -Sy grub efibootmgr dosfstools os-prober mtools
 mkdir "/boot/EFI/"
-disk=$(cat "/mnt/install/efi")
+disk=$(cat "/install/efi")
 mount /dev/$disk /boot/EFI
 grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck
 grub-mkconfig -o /boot/grub/grub.cfg
