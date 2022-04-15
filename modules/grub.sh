3502805135ced4c1792dc869638243d8a3d466c0
ReD <jakob.hillers@gmail.com>
2022-04-15 18:11:10 +0200

    [fix] /install/grub to /install/bootloader corrected

--- modules/grub.sh
+++ modules/grub.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-disk=$(cat "/install/grub")
+
+disk=$(cat "/install/bootloader")
 pacman -Sy grub
 grub-install --force $disk
 grub-mkconfig -o /boot/grub/grub.cfg
+read -p "Bootloader applied on $disk (press enter to continue)"
